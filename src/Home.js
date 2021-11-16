6ffcf29212417a3ab53a2f14e660d5bf462a85e5
Yuvraj Agarkar <yuvraj.agarkar@gmail.com>
2021-11-16 14:55:30 +0530

    added updates feed reference in Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AboutHome from "./components/AboutHome";
 import FirstBannerHome from "./components/FirstBannerHome";
 import TestimonialsHome from "./components/TestimonialsHome";
+import { useLocation, useHistory } from "react-router-dom";
 
 function Home() {
+  // check if there is status and token query and redirect to /feed passing same args
+  const history = useHistory();
+  const { search } = useLocation();
+  useEffect(() => {
+    let query = search.slice(1);
+    const queryArgs = search.split("&");
+    const status = queryArgs[0]?.split("=")[1];
+    const token = queryArgs[1]?.split("=")[1];
+    if (status && token) {
+      // take them to feed
+      history.replace(`/feed${search}`);
+    }
+  }, []);
+
   return (
     <div>
       <FirstBannerHome />
