e5b6d55644c1e48cb6b75dcad75437d746919b07
Yuvraj Agarkar <yuvraj.agarkar@gmail.com>
2022-04-13 22:05:29 +0530

    added spinner to teachers page

--- src/Teachers.js
+++ src/Teachers.js
@@ -2,18 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import "./Teahers.css";
 import SingleTeacher from "./SingleTeacher.js";
 import { getTeachers } from "./configs/networkManager";
-
+import Spinner from "./Spinner";
 function Teachers() {
   const searchRef = useRef();
   const [searchedTeachers, setSearchedTeachers] = useState([]);
-  // const [showSpinner, setShowSpinner] = useState(true);
+  const [showSpinner, setShowSpinner] = useState(false);
   const crossClicked = () => {
     searchRef.current.value = "";
   };
   useEffect(() => {
     (async () => {
+      setShowSpinner(true);
       const teachers = await getTeachers();
       setSearchedTeachers(teachers.teachers);
+      setShowSpinner(false);
     })();
   }, []);
   const searchChanged = async (event) => {
@@ -39,6 +41,7 @@ function Teachers() {
           <SingleTeacher teacher={el} />
         ))}
       </div>
+      {showSpinner && <Spinner />}
     </div>
   );
 }
